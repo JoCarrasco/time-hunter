ec6216415f0899d8dd5c623ed6ea9819bbb5b59a
JoCarrasco <jogregcarrasco@gmail.com>
2022-09-19 20:30:23 -0400

    [TimeDisplay]: Add TimerDisplay component

---
+++ src/core/components/display/TimerDisplay/TimerDisplay.cy.tsx
@@ -0,0 +1,97 @@
+import TimerDisplay from './TimerDisplay';
+import {
+  CY_SELECTOR_TIMER_DISPLAY_INPUT,
+  DEFAULT_TIMER_DISPLAY_STRING,
+  MOCK_TIMER_DISPLAY_EXPECTED_FORMATTED_TEXT,
+} from './TimerDisplay.contants';
+
+describe('CoreComponents/TimerDisplay', () => {
+  it('mounts', () => {
+    cy.mount(<TimerDisplay />);
+  });
+
+  it(`In Idle state has value of "${DEFAULT_TIMER_DISPLAY_STRING}"`, () => {
+    cy.mount(<TimerDisplay />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT).should(
+      'have.value',
+      DEFAULT_TIMER_DISPLAY_STRING,
+    );
+  });
+
+  it('Select all the characters when clicked', () => {
+    cy.mount(<TimerDisplay />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT)
+      .click()
+      .then((input) => {
+        // Get the HTML DOM Node of the Input
+        const inputHTML = input.get(0) as HTMLInputElement;
+        const valueLengh = inputHTML.value.length;
+
+        cy.wrap(
+          inputHTML.selectionStart === 0 &&
+            inputHTML.selectionEnd === valueLengh,
+        );
+      })
+      .should('equal', true);
+  });
+
+  it('Return to default value when empty', () => {
+    cy.mount(<TimerDisplay />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT)
+      .clear()
+      .blur()
+      .then((input) => {
+        cy.wrap(input.val() === DEFAULT_TIMER_DISPLAY_STRING);
+      })
+      .should('equal', true);
+  });
+
+  it('Should correct value to proper format using only numbers', () => {
+    cy.mount(<TimerDisplay />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT)
+      .click()
+      .type('120.5')
+      .blur()
+      .then((input) => {
+        cy.wrap(input.val() === MOCK_TIMER_DISPLAY_EXPECTED_FORMATTED_TEXT);
+      })
+      .should('equal', true);
+  });
+
+  it('Should correct value to proper format using text', () => {
+    cy.mount(<TimerDisplay />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT)
+      .click()
+      .type('02:00:30')
+      .blur()
+      .then((input) => {
+        cy.wrap(input.val() === MOCK_TIMER_DISPLAY_EXPECTED_FORMATTED_TEXT);
+      })
+      .should('equal', true);
+  });
+
+  it('Should maintain formatted value', () => {
+    cy.mount(<TimerDisplay />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT)
+      .type('120.5')
+      .blur()
+      .click()
+      .blur()
+      .then((input) => {
+        cy.wrap(input.val() === MOCK_TIMER_DISPLAY_EXPECTED_FORMATTED_TEXT);
+      })
+      .should('equal', true);
+  });
+
+  it('Should show proper duration passing a Date object', () => {
+    const date = new Date();
+    date.setHours(date.getHours() - 2);
+    date.setMinutes(date.getMinutes() - 30);
+    cy.mount(<TimerDisplay start={date} />);
+    cy.get(CY_SELECTOR_TIMER_DISPLAY_INPUT)
+      .then((input) => {
+        cy.wrap(input.val() === '02:30:00');
+      })
+      .should('equal', true);
+  });
+});
---
+++ src/core/components/display/TimerDisplay/TimerDisplay.tsx
@@ -0,0 +1,83 @@
+import TextField from '@mui/material/TextField';
+import { useRef, useState } from 'react';
+import { TimeHelper } from '../../../helpers/TimeHelper';
+import {
+  DEFAULT_TIMER_DISPLAY_STRING,
+  CY_SELECTOR_VAL_TIMER_DISPLAY_INPUT,
+  CY_SELECTOR_VAL_TIMER_DISPLAY_WRAPPER,
+} from './TimerDisplay.contants';
+import './TimerDisplay.scss';
+
+interface ITimerDisplayProps {
+  start?: Date;
+}
+
+const TimerDisplay = (props: ITimerDisplayProps) => {
+  const [val, setVal] = useState(
+    props.start
+      ? TimeHelper.dateRangeToDurationString(props.start, new Date())
+      : DEFAULT_TIMER_DISPLAY_STRING,
+  );
+  const [lastVal, setLastVal] = useState(val);
+  const inputRef = useRef<HTMLInputElement>();
+
+  const handleBlur = () => {
+    if (inputRef.current) {
+      if (inputRef.current.value !== lastVal) {
+        const inputVal = inputRef.current.value;
+        if (inputVal === '') {
+          inputRef.current.value = DEFAULT_TIMER_DISPLAY_STRING;
+        } else {
+          if (TimeHelper.isOnlyNumbers(inputVal) && !inputVal.includes(':')) {
+            const durationStr =
+              TimeHelper.convertNumbersToDurationString(inputVal);
+            inputRef.current.value = durationStr;
+          } else {
+            const timeObj = TimeHelper.durationStringToFullTimeObject(inputVal);
+            if (timeObj !== null) {
+              inputRef.current.value = `${timeObj.hours}:${timeObj.minutes}:${timeObj.seconds}`;
+            } else {
+              inputRef.current.value = lastVal;
+            }
+          }
+        }
+
+        setLastVal(inputRef.current.value);
+      }
+    }
+  };
+
+  const handleFocus = () => {
+    if (inputRef.current) {
+      const valLength = inputRef.current.value.length;
+      inputRef.current.setSelectionRange(0, valLength);
+    }
+  };
+
+  return (
+    <div
+      className="timer-display-text-field-wrapper"
+      data-cy={CY_SELECTOR_VAL_TIMER_DISPLAY_WRAPPER}
+    >
+      <TextField
+        hiddenLabel
+        id="filled-hidden-label-small"
+        defaultValue={val}
+        variant="filled"
+        size="small"
+        inputRef={inputRef}
+        inputProps={{
+          'data-cy': CY_SELECTOR_VAL_TIMER_DISPLAY_INPUT,
+        }}
+        InputProps={{
+          disableUnderline: true,
+          onFocus: handleFocus,
+          onBlur: handleBlur,
+        }}
+        fullWidth
+      />
+    </div>
+  );
+};
+
+export default TimerDisplay;
